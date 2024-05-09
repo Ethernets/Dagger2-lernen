0e63b20376e6e1250c146137bea819bd748a54ca
Ethernets <ethernetss@gmail.com>
2024-05-09 17:05:29 +0200

    Lazy initialization of global services

--- app/src/main/java/com/example/daggerlernen/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/example/daggerlernen/common/composition/AppCompositionRoot.kt
@@ -1,5 +1,6 @@
 package com.example.daggerlernen.common.composition
 
+import androidx.annotation.UiThread
 import com.example.daggerlernen.Constants
 import com.example.daggerlernen.networking.StackoverflowApi
 import com.example.daggerlernen.questions.FetchQuestionDetailsUseCase
@@ -7,12 +8,17 @@ import com.example.daggerlernen.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@UiThread
 class AppCompositionRoot {
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    private var _retrofit: Retrofit? = null
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private val stackoverflowApi: StackoverflowApi by lazy { retrofit.create(StackoverflowApi::class.java) }
     val fetchQuestionsUseCase get() = FetchQuestionsUseCase(stackoverflowApi)
     val fetchQuestionDetailsUseCase get() = FetchQuestionDetailsUseCase(stackoverflowApi)
 }
